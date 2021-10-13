e579341922cec625bf51b0782d7182c212ff74ad
joealsaaidah <alsaaidahyousef@gmail.com>
2021-10-14 01:23:58 +0800

    home and icons sections

--- js/script.js
+++ js/script.js
@@ -29,3 +29,29 @@ window.onload = () => {
     document.querySelector(".header .header-2").classList.remove("active");
   }
 };
+
+var swiper = new Swiper(".books-slider", {
+  /* slidesPerView: 1,
+  spaceBetween: 10, */
+  /* pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }, */
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
