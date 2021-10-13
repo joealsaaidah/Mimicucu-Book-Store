9591237e4f7ea3b6728e386ff096f9fdabb0c30a
joealsaaidah <alsaaidahyousef@gmail.com>
2021-10-14 05:39:53 +0800

    featured section

--- js/script.js
+++ js/script.js
@@ -55,3 +55,30 @@ var swiper = new Swiper(".books-slider", {
     },
   },
 });
+var swiper = new Swiper(".featured-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
